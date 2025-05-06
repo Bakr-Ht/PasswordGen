e251cad4b703c63c862535443aa90de1936f9782
Bakr <aboubakrhouta@gmail.com>
2025-05-06 18:34:28 +0200

    Add files via upload

---
+++ PasswordGen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Simple Password Generator
+
+echo -e  "**********  THE LIMIT LENGHT IS 64 CHARACTER  ***********\n "
+read -p  "How Many Password You To Genarate : " NUM
+read -p "Entetr The Length Of The Password : " PASS_LENGTH
+
+for p in $(seq $NUM);
+do
+  echo -n "Generated password : ";openssl rand -base64 48  | cut -c1-$PASS_LENGTH
+done
